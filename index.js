e53c7eb9d606a0eb2dac247d6f42c165c93659f5
Dave Beck <dbeck2@outlook.com>
2018-06-12 16:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
+var kitten = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   var name = "Ralph";
   return kittens.push(name);
